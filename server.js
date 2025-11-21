fe88d3741269c2f7ccbc314dfa69d0c9c7fba993
cassyyy14 <dumpsandra533@gmail.com>
2025-11-21 17:16:13 +0000

    Update message

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Ma. Cassandra E. Matosinos");
-  res.write("BA-4102");
-  res.write("Respect yourself enough to walk away from anything that no longer grows you.");
+  res.write("Ma. Cassandra E. Matosinos\n");
+  res.write("BA-4102\n");
+  res.write("Respect yourself enough to walk away from anything that no longer grows you.\n");
   res.end();
 });
 
